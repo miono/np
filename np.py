c5c98bb5c2d8249be1fc7002e30ff0eadfa99956
Markus Nilsson <markus.nilsson@bonniergaming.com>
2016-03-15 00:02:32 +0100

    Changing method from findall to find, since we're just interested in one song

--- np.py
+++ np.py
@@ -16,7 +16,7 @@ except IndexError:
 result = urllib2.urlopen("%s/2.0/?method=user.getrecenttracks&limit=1&user=%s&api_key=%s" % (api_url, user, api_key)).read()
 dom = ET.fromstring(result)
 
-artist = dom.findall("recenttracks/track/artist")[0].text
-song = dom.findall("recenttracks/track/name")[0].text
+artist = dom.find("recenttracks/track/artist").text
+song = dom.find("recenttracks/track/name").text
 
 print "%s - %s" % (artist, song)
